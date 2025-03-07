cef8d47b1a905b2f8cf3008acb7e8e78e65d0ebf
snwrr <ylala2910@gmail.com>
2025-03-07 16:55:26 +0100

    (feat) création du projet Maven

---
+++ bellman-ford-tests/src/main/java/com/tests/bellman_ford_tests/App.java
@@ -0,0 +1,10 @@
+package com.tests.bellman_ford_tests;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ bellman-ford-tests/src/test/java/com/tests/bellman_ford_tests/AppTest.java
@@ -0,0 +1,19 @@
+package com.tests.bellman_ford_tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
