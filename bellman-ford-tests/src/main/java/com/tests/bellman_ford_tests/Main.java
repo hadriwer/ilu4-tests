62900f2eb08a18f5891f747b338ceffa27cf7e74
skytooot <matheo.vintrou@gmail.com>
2025-03-21 15:49:15 +0100

    (feat) fin de la question 3, toutes les instructions sont couvertes

--- bellman-ford-tests/src/main/java/com/tests/bellman_ford_tests/Main.java
+++ bellman-ford-tests/src/main/java/com/tests/bellman_ford_tests/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		RunTest.runTestCase("tests_cases.txt");
+		RunTest.runTestCase("question3.txt");
+		RunTest.runTestCase("negative_weight_cycle.txt");
 	}
 
 }
